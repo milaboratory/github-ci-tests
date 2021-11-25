57f7c098f9be9bb9be6873c4cd34d6288096a4c2
Korenevskiy Denis <denkoren@yandex.ru>
2021-11-25 15:13:21 +0300

    Fake-app builder script

---
+++ scripts/fake-app.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+
+dst_dir="${1}"
+target_platform="${2:-${RUNNER_OS:-}}"
+
+echo "Fake App build was triggered for '${dst_dir}' (${target_platform})"
+
+mkdir -p "${dst_dir}"
+
+ext=""
+case "${target_platform}" in
+  "Linux")
+    ext=".AppImage"
+    ;;
+  "Windows")
+    ext=".exe"
+    ;;
+  "macOS")
+    ext=".dmg"
+    ;;
+  "JavaScript")
+    ext=".js"
+esac
+
+:> "${dst_dir}/fake-app${ext}"

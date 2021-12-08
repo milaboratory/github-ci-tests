7c626283d7dbf72698b0db23a4a5e2144c9f250e
Korenevskiy Denis <denkoren@yandex.ru>
2021-12-08 13:33:08 +0300

    Expect other env variables with auth data for github maven repo

--- gradle/stub-lib/build.gradle.kts
+++ gradle/stub-lib/build.gradle.kts
@@ -40,8 +40,8 @@ publishing {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/milaboratory/releases")
                 credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
+                    username = System.getenv("GH_MAVEN_AUTH_USER")
+                    password = System.getenv("GH_MAVEN_AUTH_TOKEN")
                 }
             }
         }

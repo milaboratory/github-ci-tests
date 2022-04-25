97f8ce8d96972501dd56f931228c8045bf64df07
Korenevskiy Denis <denkoren@yandex.ru>
2022-04-25 14:43:18 +0300

    feat: add faulty test to stub/lib stub tests

--- gradle/stub-lib/src/test/java/com/milaboratory/stub/lib/LibraryTest.java
+++ gradle/stub-lib/src/test/java/com/milaboratory/stub/lib/LibraryTest.java
@@ -5,7 +5,12 @@ import static org.junit.Assert.*;
 
 public class LibraryTest {
     @Test
-    public void testStub() {
+    public void testStubSuccess() {
         assertEquals(10, 10);
+    }
+
+    @Test
+    public void testStubFailure() {
+        assertEquals(10, 8);
     }
 }
